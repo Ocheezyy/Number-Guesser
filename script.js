6303649a44efe5aa304773fc6d87a173a125a358
Ocheezyy <miniod2018@gmail.com>
2021-01-27 15:40:13 -0500

    Updated generateTarget() solving target always being 0

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let currentRoundNumber = 1;
 
 // Write your code below
 function generateTarget(){
-  return Math.floor(Math.random(0, 9))
+  return Math.floor(Math.random(0, 9) * 10)
 }
 
 function compareGuesses(humanGuess, cpuGuess, targetNum){
